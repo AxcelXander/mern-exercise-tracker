f6eb2b97409af9fe84fc4c68abde004b527fed62
AxcelXander <tyz666@bu.edu>
2022-01-29 00:15:03 -0800

    somechanges

---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class ExercisesList extends Component {
+    render() {
+        return (
+            <div>
+              <p>YOu are on the Excercises List component!</p>
+            </div>
+        )
+    }
+}
